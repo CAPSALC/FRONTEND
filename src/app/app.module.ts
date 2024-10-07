0c35cfb434649b31d41d251ae034badeb823ac16
Cesar Celis <cocelis@outlook.com>
2024-10-06 20:58:01 -0600

    Initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeesListComponent
   ],
   imports: [
     BrowserModule,
